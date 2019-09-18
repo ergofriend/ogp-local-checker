12a8a7496063d4ef6735acc27385d017b45c4aa9
ErgoFriend <erg.friend@gmail.com>
2019-09-18 15:18:22 +0900

    fixed files

--- src/components/facebook.tsx
+++ src/components/facebook.tsx
@@ -1,9 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Facebook: React.FC = () => {
-    return (
-        <div className="Facebook"></div>
-    )
-}
+  return <div className="Facebook"></div>;
+};
 
 export default Facebook;
