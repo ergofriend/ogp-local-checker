ce1eb1e4b916323c2cecc624f217a31b3ae210f9
ErgoFriend <erg.friend@gmail.com>
2019-10-06 16:18:13 +0900

    renew style

--- components/twitter.tsx
+++ components/twitter.tsx
@@ -18,9 +18,9 @@ const Twitter: React.FC<props> = props => {
       />
       <ContentStyle>
         <TitleStyle>{props.title}</TitleStyle>
-        <p>
+        <DescriptionStyle>
           {props.description}
-        </p>
+        </DescriptionStyle>
         <p>{props.site}</p>
       </ContentStyle>
     </TwitterStyle>
@@ -28,7 +28,7 @@ const Twitter: React.FC<props> = props => {
 };
 
 const TwitterStyle = Styled.div`
-  max-width: 600px;
+  max-width: 510px;
   border-radius: .85714em;
   border-width: 1px;
   border-style: solid;
@@ -45,11 +45,13 @@ const ImgStyle = Styled.img`
   /* padding: 1rem; */
   border-radius: 25px 25px 0 0;
   width: 100%;
-  height: 330px;
-  object-fit: cover; 
+  max-height: 266px;
+  /* height: 330px; */
+  object-fit: cover;
 `;
 
 const ContentStyle = Styled.div`
+  max-height: 100px;
   padding-left: 1em;
   padding-right: 1em;
   padding: .75em;
@@ -74,5 +76,33 @@ const TitleStyle = Styled.h2`
   direction: ltr;
   unicode-bidi: isolate;
 `;
+
+const DescriptionStyle = Styled.p`
+  overflow: hidden;
+  height: 2.8em;
+  font-size: 14.9px;
+  line-height: 1.4;
+
+  position: relative;
+  padding-right: 1em;
+  overflow: hidden;
+  &::before {
+    content: "...";
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    display: inline-block;
+    width: 1em;
+  }
+  &::after {
+    content: "";
+    position: relative;
+    right: -1em;
+    float: right;
+    width: 1em;
+    height: 100%;
+    background-color: inherit;
+  }
+`
 
 export default Twitter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,61 +2,110 @@ import React, { useState, useEffect } from "react";
 import Styled from "styled-components";
 
 import "rsuite/dist/styles/rsuite-default.css";
-import { Button, InputGroup, Input, Icon } from "rsuite";
+import { Nav, Form, FormGroup, InputGroup, Input, Icon, Loader } from "rsuite";
 
 import Twitter from "../components/twitter";
+import is_url from "../util/is_url"
 
 const Index: React.FC = () => {
-  const [result, setResult] = useState({
-    header: null,
-    status: "loading",
-    error: null
-  });
+  const [loading, setLoading] = useState(false);
+  const [isUrl, setIsUrl] = useState(true);
   const [url, setUrl] = useState("");
+  const [ogp, setOGP] = useState({
+    card: "summary",
+    image: "https://tento.app/sns.png",
+    title: "Tento - 集まろう！ 学生の新しいコミュニティー",
+    description: "Tentoとは学生のみが利用する学生求人サイトです。あなたの興味のあるこや、やりたいことをみんなでシェアして取り組める!イベント開催告知から開発メンバー募集までなんでもあり！",
+    site: "location.hostname"
+  })
 
   useEffect(() => {
-    fetch(url, { mode: "no-cors" })
-      .then(response => {
-        console.log("response:", response);
-        return response.json();
-      })
-      .then(response =>
-        setResult({ header: response, status: "loaded", error: null })
-      )
-      .catch((error: any) =>
-        setResult({ header: null, status: "error", error: error })
-      );
-    console.log(result);
-  }, [result, setUrl, url]);
+    // Update the document title using the browser API
+    if (is_url(url)) {
+      setIsUrl(true)
+      setLoading(true)
+      getInfo(url)
+    } else {
+      setIsUrl(false)
+    }
+  },[url]);
+
+  const clearURL = () => {
+    setUrl("")
+  }
+
+  const getHostname = (url: string) => {
+    // use URL constructor and return hostname
+    return new URL(url).hostname;
+  }
+
+  const getInfo = async (url: string) => {
+    const response = await fetch(location.href + "api?url=" + url)
+    const ogp = await response.json();
+    console.log("response:", ogp);
+    const ogpData = {
+      card: ogp.twitter_card,
+      image: ogp.twitter_image ? ogp.twitter_image : ogp.og_image,
+      title: ogp.og_title,
+      description: ogp.og_description,
+      site: getHostname(ogp.og_url ? ogp.og_url : ogp.url)
+    }
+    setOGP(ogpData)
+    setLoading(false)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>You can confirm the SNS OGP with the local computer.</p>
+        <Nav >
+          <Nav.Item eventKey="home" icon={<Icon icon="home" />}>
+            Home
+      </Nav.Item>
+          <Nav.Item eventKey="news">News</Nav.Item>
+          <Nav.Item eventKey="solutions">Solutions</Nav.Item>
+          <Nav.Item eventKey="products">Products</Nav.Item>
+          <Nav.Item eventKey="about">About</Nav.Item>
+        </Nav>
       </header>
-      <iframe title="test" src={url} height="100px"></iframe>
       <InputStyle>
+        <Form fluid>
+          <FormGroup>
         <InputGroup inside>
+          {(() => {
+            return url.length > 0 ?
+              <InputGroup.Button onClick={clearURL}>
+                <Icon icon="close" />
+              </InputGroup.Button>
+              : null
+            }
+          )()}
           <Input
             name="url"
-            onChange={value => {
-              setUrl(value as string);
-              console.log(value);
-            }}
+            type="url"
+            size="md"
+            value={url}
+            onChange={value => setUrl(value)}
           />
-          <InputGroup.Button>
-            <Icon icon="search" />
-          </InputGroup.Button>
-        </InputGroup>
+          {(() => {
+            return loading
+            ? <InputGroup.Button>
+              <Loader content="Loading..." />
+            </InputGroup.Button>
+            : <InputGroup.Button>
+                <Icon icon="search" />
+              </InputGroup.Button>
+          })()}
+            </InputGroup>
+            <Twitter card={ogp.card} image={ogp.image} title={ogp.title} description={ogp.description} site={ogp.site}></Twitter>
+          </FormGroup>
+        </Form>
       </InputStyle>
-      <Twitter></Twitter>
-      <Button appearance="primary"> Hello world </Button>
     </div>
   );
 };
 
 const InputStyle = Styled.div`
-  width: 60vw;
+  padding: 1rem 2rem;
 `;
 
 export default Index;
